f34d525891fe477e170bdb25e79b3412ea520e36
jrhauser <jrh00045@mix.wvu.edu>
2025-02-18 20:03:59 -0500

    more files generated and first draft of converter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 import numpy as np
 import os
 import csv
-
+import multiprocessing
 def testRunner(varCount, conCount, timing):
     hornex = open('hornex.txt', 'w+')
     headerGen(varCount, conCount, hornex)
@@ -16,41 +16,61 @@ def testRunner(varCount, conCount, timing):
     subprocess.run(['gcc', '-std=c99', '-o', 'lift', 'lift.c'])
     proc = subprocess.run([str('./lift hornex.txt ' + timing)], shell=True)
 
+
+def getFileNum(hornPath):
+    os.makedirs(hornPath, exist_ok=True)
+    directoryContents = os.listdir(hornPath)
+    num = 0
+    fileNums = []
+    for file in directoryContents:
+        if file[-6] != '_':
+            fileNums.append(int(file[-6:-4]))
+        else:
+            fileNums.append(int(file[-5]))
+    fileNums.sort()
+    if not fileNums:
+        num = 0
+    else:
+        num = fileNums[-1]
+    return num
+
+
+
 def systemGenerator(varCount, conCount, timing, feasNum):
-    systems = feasNum * 2 
+    systems = feasNum * 2
     i = 0
-    while (i < systems):
-        if (i < feasNum):
+    hornPath = "test_systems/" + str(varCount) + "/" + str(conCount) + "/feasible/"
+    fileNum = getFileNum(hornPath)
+    feas = True
+    if fileNum == 20:
+         hornPath = "test_systems/" + str(varCount) + "/" + str(conCount) + "/infeasible/"
+         fileNum = getFileNum(hornPath)
+         feas = False
+    j = systems - fileNum
+    while (i < j):
+        fileNum += 1
+        if fileNum > 20 and feas:
+             fileNum = 1
+             feas = False
+        elif fileNum > 20 and not feas:
+            break
+        if feas:
              hornPath = "test_systems/" + str(varCount) + "/" + str(conCount) + "/feasible/"
         else:
              hornPath = "test_systems/" + str(varCount) + "/" + str(conCount) + "/infeasible/"
-        os.makedirs(hornPath, exist_ok=True)
-        directoryContents = os.listdir(hornPath)
-        num = 0
-        fileNums = []
-        for file in directoryContents:
-            if file[-6] != '_':
-                fileNums.append(int(file[-6:-4]))
-            else:
-                fileNums.append(int(file[-5]))
-        fileNums.sort()
-        if not fileNums:
-            num = 0
-        else:
-            num = fileNums[-1]
-        fullPath = hornPath + str(varCount) + "_" + str(conCount) + "_" + str(num + 1) + ".txt"
+             os.makedirs(hornPath, exist_ok=True)
+        fullPath = hornPath + str(varCount) + "_" + str(conCount) + "_" + str(fileNum) + ".txt"
         hornex = open(fullPath, 'w+')
         headerGen(varCount, conCount, hornex)
         conTable = []
         for _ in range(conCount):
             constraintGen(varCount, hornex, conTable, conCount)
         hornex.close()
-        subprocess.run(['gcc', '-std=c99', '-o', 'lift', 'lift.c'])
         subprocess.run(['./lift', hornex.name, timing])
         df = pd.read_csv(timing)
         feasibleCol = df['feasible'].tolist()
         i += 1
-        if (i < feasNum and feasibleCol[-1] == 0):
+        if (feas and feasibleCol[-1] == 0):
             f = open(timing, 'r+')
             lines = f.readlines()
             if len(lines) != 1:
@@ -61,7 +81,8 @@ def systemGenerator(varCount, conCount, timing, feasNum):
             f.close()
             os.remove(fullPath)
             i -= 1
-        elif (i > feasNum and feasibleCol[-1] == 1):
+            fileNum -= 1
+        elif (not feas and feasibleCol[-1] == 1):
             f = open(timing, 'r+')
             lines = f.readlines()
             if len(lines) != 1:
@@ -72,6 +93,7 @@ def systemGenerator(varCount, conCount, timing, feasNum):
             f.close()
             os.remove(fullPath)
             i -= 1
+            fileNum -= 1
         
 
 
@@ -157,37 +179,72 @@ fiveHundreds = [
     ]
 thousands = [
         (1000, 2000),
-        (1000, 4500),
+        (1000, 4500)
+    ]
+thousandsPT2 = [
         (1000, 10000),
         (1000, 1000000)
     ]
 twoThousand = [
         (2000, 5000),
-        (2000, 4000),
+        (2000, 4000)
+    ]
+twoThousandPT2 = [
         (2000, 22000),
         (2000, 4000000)
     ]
 fiveThousand = [
         (5000, 5000),
-        (5000, 10000),
+        (5000, 10000)
+    ]
+fiveThousand1 = [
         (5000, 60000),
         (5000, 25000000)
     ]
-varCounts = [hundreds, twoHundreds, fiveHundreds, thousands, twoThousand, fiveThousand]
+fiveThousand2 = [
+        (5000, 25000000)
+    ]
+varCounts = [fiveHundreds, thousands, thousandsPT2, twoThousand, twoThousandPT2, fiveThousand, fiveThousand1, fiveThousand2]
 testRuns = 20
-for varCount in varCounts:
+timingNum = 1
+""" for varCount in varCounts:
     for conCount in varCount:
-            p = "timing/" + datetime.datetime.now().strftime("%Y_%m_%d/")
+            p = "generatorTiming" + str(timingNum) +"/" + datetime.datetime.now().strftime("%Y_%m_%d/")
             os.makedirs(p, exist_ok=True)
             with open(p + str(conCount[0]) + "_vars_" + str(conCount[1]) + "_cons.csv", 'w+') as timing:
                 timing.write("feasible,beginning_to_start,start_to_solution,total,zero_solution,lifts\n")
             
+
+
             systemGenerator(conCount[0], conCount[1], timing.name, testRuns)
             
             #for _ in range(testRuns): 
                 #testRunner(conCount[0], conCount[1], timing.name) 
             #statsGenerator(timing)
+ """
 
 
 
 
+
+def procLoop(varCount):
+    for conCount in varCount:
+            p = "generatorTiming" + str(timingNum) +"/" + datetime.datetime.now().strftime("%Y_%m_%d/")
+            os.makedirs(p, exist_ok=True)
+            with open(p + str(conCount[0]) + "_vars_" + str(conCount[1]) + "_cons.csv", 'w+') as timing:
+                timing.write("feasible,beginning_to_start,start_to_solution,total,zero_solution,lifts\n")
+            systemGenerator(conCount[0], conCount[1], timing.name, testRuns)
+
+
+if __name__ == "__main__":
+     processes = []
+     for varCount in varCounts:
+            p = multiprocessing.Process(target=procLoop, args=(varCount,))
+            processes.append(p)
+            p.start()
+            
+            #for _ in range(testRuns): 
+                #testRunner(conCount[0], conCount[1], timing.name) 
+            #statsGenerator(timing)
+     for p in processes:
+        p.join()
